9ddadb2f9ee1c322f80a2482c64875980fef0975
unknown <danileron@gmail.com>
2018-08-12 16:13:23 +0300

    WorkQueue

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,14 +1,24 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class MainActivity {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello, app is working");
+
+        BlockingQueue<Work> doneWorkQueue = new LinkedBlockingDeque<>();
 
         Work easyWork = new Work(6);
         Work hardWork = new Work(20);
 
-        Worker bill = new Worker("Bill", 2, easyWork);
-        Worker jill = new Worker("Jill", 4, hardWork);
+        Worker bill = new Worker("Bill", 2, easyWork, doneWorkQueue);
+        Worker jill = new Worker("Jill", 4, hardWork, doneWorkQueue);
 
         bill.start();
         jill.start();
+
+        jill.join();
+        bill.join();
+
+        System.out.println(doneWorkQueue);
     }
 }
--- src/Worker.java
+++ src/Worker.java
@@ -1,13 +1,17 @@
+import java.util.concurrent.BlockingQueue;
+
 public class Worker extends Thread {
 
     private String name;
     private int skillLevel;
     private Work work;
+    private BlockingQueue<Work> queue;
 
-    public Worker(String name, int skillLevel, Work work) {
+    public Worker(String name, int skillLevel, Work work, BlockingQueue<Work> queue) {
         this.name = name;
         this.skillLevel = skillLevel;
         this.work = work;
+        this.queue = queue;
     }
 
 
@@ -27,6 +31,11 @@ public class Worker extends Thread {
             }
         }
         System.out.println("Worker " + this.getWorkerName() + " done his job " + work.toString());
+        try {
+            queue.put(work);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getWorkerName() {
